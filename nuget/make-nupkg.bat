1535cdabe3b46787710086ece1e3f19423aa9ecc
David Poirier <david.poirier@carsales.com.au>
2014-07-18 14:05:55 +1000

    Updated readme to reflect new weighting param
    Bumped version to 1.0.2
    Updated nuget

--- nuget/make-nupkg.bat
+++ nuget/make-nupkg.bat
@@ -1,4 +1,3 @@
 call "\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 msbuild /p:Configuration=Release ..\src\Showpony\Showpony.csproj
-..\tools\nuget\nuget.exe pack Showpony.1.0.1.0.nuspec
-
+..\tools\nuget\nuget.exe pack Showpony.nuspec
